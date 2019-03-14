45aae70a735df4745e091deaf4cb0236b6108c0a
xieqiupeng <xieqiupeng@gmail.com>
2019-03-14 19:14:47 +0800

    udpate

---
+++ 专业课/dependency.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# Dependency
+ln -sf /Volumes/Project/List/List-  $Path/2/List-
+ln -sf /Volumes/Project/Enum/Enum-Technology/Java  $Path/Java
+ln -sf /Volumes/Project/Algorithm/Algorithm-  $Path/Algorithm-
+
+# Root
+Path=/Volumes/Project/Rep/Age-of-University/专业课
+ln -sf /Volumes/Project/Enum/Enum-Technology/Java  $Path/Java
+
+
+
+
